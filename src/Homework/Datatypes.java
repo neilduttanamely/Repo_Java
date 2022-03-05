812d9549569c106bf08f728d3a4a60516f7a8b3d
RUDRA NEIL DUTTA <rudraneildutta@RUDRAs-MBP.fios-router.home>
2022-03-05 13:15:39 -0500

    homework 1

---
+++ src/Homework/Datatypes.java
@@ -0,0 +1,21 @@
+package Homework;
+
+public class Datatypes {
+    private String sSN;
+
+    public static void main (String[]) args) {
+
+        //Primitive
+        int age = 25;
+        age = 30;
+
+        System.out.println(age);
+        age = 35;
+        double price = 2.99;
+        boolean isMuted = false;
+        char nameofTrain = 'E';
+
+        // Non-Primitive
+        String name = "Ahsan";
+    }
+}
